ea66d1009c52b9632957e6257024ed99616e3596
okada-keiji <xenophy.keiji.okada@gmail.com>
2014-05-12 10:13:58 +0900

    テスト用ストアの削除

--- public_html/app/store/Store.js
+++
@@ -1,16 +0,0 @@
-Ext.define('OsakaTest.store.Store', {
-    extend: 'Ext.data.Store', 
-    fields: [ {name: 'id', type: 'int'}, 'first', 'last' ],
-    idProperty: 'id',
-    data: [
-        { id: 1, first: 'ジョン', last: 'レノン' },
-        { id: 2, first: 'ポール', last: 'マッカートニー' },
-        { id: 3, first: 'ジョージ', last: 'ハリソン' },
-        { id: 4, first: 'リンゴ', last: 'スター' }
-    ],
-    proxy:{
-        type:'memory'
-    }
-//model: 'osakatest.model.employee',
-//autoload: true
-});
