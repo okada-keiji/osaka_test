105ab9f807aa78be851b96b787d8384c77c2e96c
okada-keiji <xenophy.keiji.okada@gmail.com>
2014-05-14 14:25:25 +0900

    ユーザー追加用のphp（create_user.php)を現在の環境を仕様に合わせて変更

--- public_html/php/create_user.php
+++ public_html/php/create_user.php
@@ -1,51 +1,49 @@
 <?php
-// データベースへ接続
-require 'connect.php';
-
-// パラメーターをデコード
-//
-$params = get_object_vars($_POST['data']);
-$ret = array();
-foreach ($params as $param) {
-    $ret[] = createUser($param);
+if (isset($_POST['data'])) {
+    $data = json_decode($_POST['data']);
+    $ret = array();
+    foreach ($data as $record) {
+        $param = get_object_vars($record);
+        $ret[] = createUser($param);
+    }
+} else {
+    $ret = createUser($_POST);
 }
 // json 形式で返す
 echo json_encode($ret);
 
 function createUser($param) {
+    require 'connect.php';
     // DBにデータを保存
     $fmt = "
         insert into employee (
             `name`,
             `kana`,
+            `address`,
             `gender`,
             `birthday`,
-            `email`,
             `pref`,
             `mobile_phone`
         ) 
-        values ('%s', '%s', %s, '%s', '%s', '%s')
+        values ('%s', '%s', '%s', %s, '%s', '%s', '%s')
         ";
     $sql = sprintf(
         $fmt, 
-        $param->name,
-        $param->kana,
-        $param->gender,
-        $param->birthday,
-        $param->email,
-        $param->pref,
-        $param->mobile_phone,
-        $param->age
+        $param["name"],
+        $param["kana"],
+        $param["address"],
+        $param["gender"],
+        $param["birthday"],
+        $param["pref"],
+        $param["mobile_phone"]
     );
-    mysql_query($sql);
+    mysqli_query($link, $sql);
 
     // 挿入したレコードのidを取得
-    $param->id = mysql_insert_id($link);
-
+    $param["id"] = mysqli_insert_id($link);
     // レスポンスをセット
     $ret = array(
         'data' => $param,
-        'total' => count($data),
         'success' => true
     );
     return $ret;
